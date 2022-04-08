b560334607d7f55916ce006f106d3702f715ca4b
brm1996 <mehtabhargavr@gmail.com>
2022-04-08 16:22:32 +0530

    Added Factorial function with tests

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -13,34 +13,76 @@ public class Calculator {
         logger.info("Calculating Square Root of : " + var + "\n Result : " + Math.sqrt(var));
         return Math.sqrt(var);
     }
+    public long  factorial(int var){
+        long fact = 1;
+        for(int i = 1; i <= var; i++){
+            fact *= i;
+        }
+        logger.info("Factorial of the number : " + var + "\n Result is : " + fact);
+        return fact;
+    }
     public static void main(String[] args){
         Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
         double num1=0, num2=0;
+        int n1=0;
         int choice=0;
+        boolean flag;
         while(true) {
             System.out.println("-----Calculator-----");
             System.out.println("Press 1 to find square root");
+            System.out.println("Press 2 to find factorial");
             System.out.println("Press any other key to exit");
-            System.out.println("Enter your choice:");
-            try {
-                choice = scanner.nextInt();
-            } catch (InputMismatchException error) {
-                System.out.println("Enter Integer number\n" + error);
-                exit(-1);
+            flag=true;
+            while(flag) {
+                System.out.println("Enter your choice:");
+                try {
+                    choice = scanner.nextInt();
+                    flag=false;
+                } catch (InputMismatchException error) {
+                    System.out.println("Enter Integer number\n" + error);
+                    scanner.nextLine();
+                }
             }
 
             switch (choice) {
                 case 1:  // For Square Root
                     System.out.println("Square Root Operation:");
-                    System.out.print("Enter the number: ");
-                    try {
-                        num1 = scanner.nextInt();
-                    } catch (InputMismatchException error) {
-                        System.out.println("Enter Integer number\n" + error);
-                        exit(-1);
+                    flag=true;
+                    while(flag) {
+                        System.out.print("Enter the number: ");
+                        try {
+                            num1 = scanner.nextDouble();
+                            if(num1<0) {
+                                System.out.println("Square root of negative number is not real number(i.e. complex number)!");
+                                continue;
+                            }
+                            flag=false;
+                        } catch (InputMismatchException error) {
+                            System.out.println("Enter Double number.\n" + error);
+                            scanner.nextLine(); // to flush input stream
+                        }
                     }
                     System.out.println("Square Root of " + num1 + " : " + calculator.squareRoot(num1));
+                    break;
+                case 2: // for Factorial
+                    System.out.println("Factorial Operation:");
+                    flag=true;
+                    while(flag) {
+                        System.out.print("Enter the number: ");
+                        try {
+                            n1 = scanner.nextInt();
+                            if(n1<0) {
+                                System.out.println("Factorial of negative number is not possible!");
+                                continue;
+                            }
+                            flag=false;
+                        } catch (InputMismatchException error) {
+                            System.out.println("Enter Integer number.\n" + error);
+                            scanner.nextLine(); // to flush input stream
+                        }
+                    }
+                    System.out.println("Factorial of " + n1 + " : " + calculator.factorial(n1));
                     break;
                 default:
                     System.out.println("Exiting...");
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -19,4 +19,17 @@ public class CalculatorTest {
         assertNotEquals("Testing for False Positives for squareRoot", Double.NaN, c.squareRoot(84.0), delta);
     }
 
+    @Test
+    public void factorialPositive() {
+        assertEquals("Testing True Positive for factorial", 24, c.factorial(4), delta);
+        assertEquals("Testing for True Negative for factorial", 120, c.factorial(5), delta);
+    }
+
+    @Test
+    public void factorialNegative() {
+        assertNotEquals("Testing False Negative for factorial", 6, c.factorial(2), delta);
+        assertNotEquals("Testing for False Positives for factorial", 120, c.factorial(6), delta);
+    }
+
+
 }
